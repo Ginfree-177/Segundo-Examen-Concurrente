1e048e123f14668543ec4e3681bce6e27db629f5
Ginfree <christian.vazquezloe@alumno.buap.mx>
2025-10-22 20:23:33 -0600

    Monitor creado

--- Ejercicio2.cpp
+++ Ejercicio2.cpp
@@ -4,6 +4,47 @@
 #include<condition_variable>
 
 //Recordatorio del la explicacion con dibujitos
+using namespace std;
+
+class mesa
+{
+public:
+    int papeles;
+    mutex mtx;
+    condition_variable cv;
+public:
+    mesa()
+    {
+        pilaPapeles = 0;
+    }
+    void agregarHoja()
+    {
+        unique_lock<mutex> lock(mtx);
+        while (pilaPapeles == 3) {
+            cv.wait(lock);
+        }
+        mesa++;
+        if (pilaPapeles == 3) {
+            cv.notify_all();
+        }
+        lock.unlock();
+        this_thread::sleep_for(chrono::milliseconds(100 + rand() % 200));
+    }
+    void vaciarHojas()
+    {
+        unique_lock<mutex> lock(mtx);
+
+        // Espera hasta que haya 3 papeles
+        while (mesa < 3) {
+            cv.wait(lock);
+        }
+        mesa = 0;
+        cv.notify_all();
+        lock.unlock();
+
+        this_thread::sleep_for(chrono::milliseconds(150));
+    }
+};
 
 int main()
 {
